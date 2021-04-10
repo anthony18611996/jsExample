f0d50c482330412e9f23a008925a88f825ada587
anthony1861996 <vaa1861@gmail.com>
2021-04-10 22:10:07 +0300

    worked
    worked

--- src/index.js
+++ src/index.js
@@ -1,41 +1,28 @@
-function toX (container, scroll, blocks) {
-    const _container = document.getElementsByTagName(container),
-    _scroll = document.querySelector(scroll),
-    _blocks = document.querySelectorAll(blocks);
+function toX (scroll) {
+    const _scroll = document.querySelectorAll(scroll);
     let scrollX;
     let scrollLeft;
     let isWork = false;
 
-    _scroll.addEventListener("mouseup", (event) => {
+_scroll.forEach((item)=> {
+    item.addEventListener("mouseup", (event) => {
         isWork = false;
-        scrollLeft = _scroll.scrollLeft;
-        //console.log(scrollLeft);
+        scrollLeft = item.scrollLeft;
     });
-    _scroll.addEventListener("mousedown", (event) => {
+    item.addEventListener("mousedown", (event) => {
         isWork = true;
         event.preventDefault();
-        scrollX = event.clientX - _scroll.offsetLeft;
-        scrollLeft = _scroll.scrollLeft;
+        scrollX = event.clientX - item.offsetLeft;
+        scrollLeft = item.scrollLeft;
     });
-    _scroll.addEventListener("mousemove", (event) => {
+    item.addEventListener("mousemove", (event) => {
         if (!isWork) return;
         event.preventDefault();
-        let element = event.clientX - _scroll.offsetLeft;
+        let element = event.clientX - item.offsetLeft;
         let scrolling = (element - scrollX) * 2;
-        _scroll.scrollLeft = scrollLeft - scrolling;
+        item.scrollLeft = scrollLeft - scrolling;
     });
-
-    /*_blocks.forEach((item)=> {
-        let rect = item.getBoundingClientRect().x;
-        let rectStart = 0;
-        item.addEventListener("mousedown", () => {
-            _scroll.style.transform = `translateX(${rect}px)`;
-        });
-        item.addEventListener("mouseup", ()=> {
-            _scroll.style.transform = `translateX(${rectStart}px)`
-        })
-    })*/
-
+})
 }
 
-toX('scroll-view', '.scroll', '.block');
+toX('.scroll');
