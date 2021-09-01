cb0781b552b26d2fa3e82a5a1bd68f992cde5a35
anthony18611996 <52492865+anthony18611996@users.noreply.github.com>
2021-09-01 14:48:45 +0300

    lint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+'use strict';
+ 
+toX('.scroll');
+
 function toX (scroll) {
     const _scroll = document.querySelectorAll(scroll);
     let scrollX;
@@ -25,4 +29,18 @@ _scroll.forEach((item)=> {
 })
 }
 
-toX('.scroll');
+const INTERVAL = 500, MAX_VALUE = 10;
+let counter = 0, timer = null;
+
+const event = () => {
+    if (counter === MAX_VALUE) {
+        console.log('The end');
+        clearInterval(timer);
+        return
+    }
+    console.dir({ counter, date: new Date() });
+    counter++;
+}
+
+console.log('Begin');
+timer = setInterval(event, INTERVAL);
